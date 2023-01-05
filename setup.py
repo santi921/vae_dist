2902dff85269735da7d031c77d526f0859cfa852
Santiago Vargas <santiagovargas921@gmail.com>
2023-01-05 12:56:23 -0500

    dataset with processing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name = 'vae_distance',
+    version = '0.0.0',
+    packages = find_packages(),
+    author="Santiago Vargas",
+    author_email="santiagovargas921@gmail.com",
+    url="https://github.com/santi921/vae_distance",
+    description="A package for training VAEs for electric field comparison",
+
+)
